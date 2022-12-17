3939fa62085e408852dbfe6a5e6de3a95deffe48
Bepo <81547893+santo-polibatam@users.noreply.github.com>
2022-12-17 11:17:44 +0700

    WIP On Hold Ticket

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Jovan_New._PrintingSubAssyRawMaterial
+            Me.MainForm = Global.Jovan_New._PrintingWIPOnHold
         End Sub
     End Class
 End Namespace
--- _PrintingWIPOnHold.vb
+++ _PrintingWIPOnHold.vb
@@ -1,18 +1,18 @@
 ﻿Imports System.IO
 Imports System.Reflection
 Imports NiceLabel.SDK
-Public Class _PrintingSubAssyRawMaterial
+Public Class _PrintingWIPOnHold
     Dim label As ILabel
 
     Dim printers As IList(Of IPrinter)
     Dim selected_Printer As IPrinter
-    Private Sub _PrintingSubAssyRawMaterial_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub _PrintingWIPOnHold_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         InitializePrintEngine()
 
         Dim appPath As String = Application.StartupPath()
-        label = PrintEngineFactory.PrintEngine.OpenLabel(appPath & "\Label\" & "Sub Assy Raw Material.nlbl")
-
+        label = PrintEngineFactory.PrintEngine.OpenLabel(appPath & "\Label\" & "WIP On Hold Ticket.nlbl")
     End Sub
+
     Private Sub InitializePrintEngine()
         Try
             Dim sdkFilesPath As String = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "..\\..\\..\\SDKFiles")
@@ -41,34 +41,36 @@ Public Class _PrintingSubAssyRawMaterial
         label.Variables("unique_ID").SetValue(txt_Unique_id.Text)
         label.Variables("part_number").SetValue(txt_part_number.Text)
         label.Variables("part_description").SetValue(txt_Part_Description.Text)
+        label.Variables("process").SetValue(txt_Process.Text)
         label.Variables("Qty").SetValue(txt_Qty.Text)
         label.Variables("traceability").SetValue(txt_Traceability.Text)
         label.Variables("Inv_ctrl_date").SetValue(txt_Inv_crtl_date.Text)
-        label.Variables("Batch_No").SetValue(txt_Batch_no.Text)
-        label.Variables("lot_no").SetValue(txt_Lot_no.Text)
+        label.Variables("status").SetValue(txt_Status.Text)
 
         label.Variables("qr_code").SetValue(txt_QR_Code.Text)
     End Sub
     Private Sub Clear_data()
         Application.DoEvents()
+        label.Variables("jenis_ticket").SetValue("")
 
         label.Variables("unique_ID").SetValue("")
         label.Variables("part_number").SetValue("")
         label.Variables("part_description").SetValue("")
+        label.Variables("process").SetValue(txt_Process.Text)
         label.Variables("Qty").SetValue("")
         label.Variables("traceability").SetValue("")
         label.Variables("Inv_ctrl_date").SetValue("")
-        label.Variables("Batch_No").SetValue("")
-        label.Variables("lot_no").SetValue("")
+        label.Variables("status").SetValue("")
 
         label.Variables("qr_code").SetValue("")
     End Sub
 
-    Private Sub _PrintingSubAssyRawMaterial_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+    Private Sub _PrintingWIPOnHold_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         PrintEngineFactory.PrintEngine.Shutdown()
     End Sub
+
     Private Sub btn_refresh_Click(sender As Object, e As EventArgs) Handles btn_refresh.Click
         Dim appPath As String = Application.StartupPath()
-        label = PrintEngineFactory.PrintEngine.OpenLabel(appPath & "\Label\" & "Sub Assy Raw Material.nlbl")
+        label = PrintEngineFactory.PrintEngine.OpenLabel(appPath & "\Label\" & "WIP On Hold Ticket.nlbl")
     End Sub
 End Class
